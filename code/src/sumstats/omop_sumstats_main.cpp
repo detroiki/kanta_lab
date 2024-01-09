efd97410cca52d7e6654030684ba9b1ec875ed84
Kira Detrois <kira.detrois@helsinki.fi>
2024-01-09 11:20:20 +0200

    Chage in delimieter and creating of omop ident

--- code/src/sumstats/omop_sumstats_main.cpp
+++ code/src/sumstats/omop_sumstats_main.cpp
@@ -1,17 +1,18 @@
 #include "../header.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     std::string res_path = argv[1];
+    std::string date = argv[2];
 
     // Reading in maps for new omop concepts
     std::unordered_map<std::string, std::vector<double>> omops;
     std::unordered_map<std::string, std::unordered_set<std::string>> omop_indvs;
 
     // Reading
-    char delim = ',';
+    char in_delim = '\t';
+    char out_delim = '\t';
     std::string line;
     int first_line = 1; // Indicates header line
     int n_lines = 0;
@@ -21,32 +22,29 @@ int main(int argc, char *argv[])
             continue;
         }
 
-        std::vector<std::string> line_vec(splitString(line, delim));
+        std::vector<std::string> line_vec(split(line, &in_delim));
         std::string finregid = line_vec[0];
         std::string lab_date_time = line_vec[1];
         std::string service_provider_name = line_vec[2];
-        std::string lab_id = remove_chars(line_vec[3], ' ');
+        std::string lab_id = line_vec[3];
         std::string lab_id_source = line_vec[4];
-        std::string lab_abbrv = remove_chars(line_vec[5], ' ');
-        std::string lab_value = remove_chars(line_vec[6], ' ');
-        std::string lab_unit = remove_chars(line_vec[7], ' ');
+        std::string lab_abbrv = line_vec[5];
+        std::string lab_value = line_vec[6];
+        std::string lab_unit = line_vec[7];
         std::string omop_id = line_vec[8];
         std::string omop_name = line_vec[9];
-        std::string lab_abnormality = remove_chars(line_vec[10], ' ');
-        std::string ref_value_text = line_vec[11];
-        std::string data_system = line_vec[12];
-        std::string data_system_ver = line_vec[13];
+        std::string lab_abnormality = line_vec[10];
+        std::string measure_status = line_vec[11];
+        std::string ref_value_text = line_vec[12];
 
         if (!(omop_id == "NA" || lab_value == "NA")) {
-            add_quotation(omop_name, delim); add_quotation(lab_unit, delim);
-            std::string omop_identifier = concat_string(std::vector<std::string>({omop_id, omop_name, lab_unit}), ",");
+            std::string omop_identifier = get_omop_identifier(omop_id, omop_name, lab_unit, out_delim);
             omops[omop_identifier].push_back(std::stod(lab_value));
             omop_indvs[omop_identifier].insert(finregid);
         }
         n_lines++; write_line_update(n_lines, begin);
     }
 
-    write_omop_sumstats(omops, omop_indvs, res_path);
-
+    write_omop_sumstats(omops, omop_indvs, res_path, date, out_delim);
     write_end_run_summary(begin);
 }
